9e2c006987c0c8c2f7a8ac5ec29c7708f5928e23
Matthew Chase <antishow@gmail.com>
2017-11-01 20:37:25 -0400

    Add basic title screen

---
+++ Assets/TitleScreenController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour {
+
+	public void OnClickNewGame(){
+		Debug.Log("New Game");
+		SceneManager.LoadScene("Main");
+	}
+
+	public void OnClickQuit(){
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+}
