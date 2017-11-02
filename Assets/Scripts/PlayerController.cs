624cabcc7626bc6ca4aab4feef2562a1e4e02f7f
Matthew Chase <antishow@gmail.com>
2017-11-02 09:01:17 -0400

    Add bullet prefab. Probably don't need to keep this but it's good demo net code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
     void Update()
     {
 		if(!isLocalPlayer){
@@ -14,7 +17,20 @@ public class PlayerController : NetworkBehaviour
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			CmdFire();
+		}
     }
+
+	[Command]
+	void CmdFire(){
+		GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+		NetworkServer.Spawn(bullet);
+
+		Destroy(bullet, 2.0f);
+	}
 
 	public override void OnStartLocalPlayer()
     {
