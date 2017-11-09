2b5da641bfb404c5011c716e79219691cf2d14ae
dmensinger <dan.mensinger@gmail.com>
2017-11-08 23:55:38 -0500

    TextController to manage UI on prefabs

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+    public static TextController instance;
+
+    void Awake() {
+        instance = this;
+    }
+
+    public Text ballText;
+
+}
