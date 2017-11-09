4f64901595b4185620dae2b84ac03a5c52e5adf9
Dan Mensinger <dan@vtldesign.com>
2017-11-09 16:51:31 -0500

    Add ThrowSpeed and adjust grabbed items transform to Hands forward position
    Items picked up now remain in the position they were on the ground and are thrown forward.

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class HandController : MonoBehaviour {
 	public GameObject Hand;
 	public GameObject HeldObject;
+	public float ThrowSpeed;
 
 	public void GrabItem(GameObject itemToGrab){
 		HeldObject = itemToGrab;
 
 		itemToGrab.transform.parent = Hand.transform;
 		itemToGrab.transform.localPosition = new Vector3(0, 0, 0);
-		itemToGrab.transform.localRotation = Quaternion.Euler(0,0,0);
+		// itemToGrab.transform.localRotation = Quaternion.Euler(0,0,0);
 
 		itemToGrab.GetComponent<Rigidbody>().isKinematic = true;
 	}
@@ -19,8 +20,8 @@ public class HandController : MonoBehaviour {
 	public void ReleaseItem(){
 		HeldObject.transform.parent = transform.parent;
 		Vector3 TossDirection =
-			(HeldObject.transform.forward * 8.0f) +
-			(HeldObject.transform.up * 3.0f);
+			(Hand.transform.forward * ThrowSpeed) +
+			(Hand.transform.up * 3.0f);
 		Rigidbody rb = HeldObject.GetComponent<Rigidbody>();
         rb.isKinematic = false;
 		rb.AddForce(TossDirection, ForceMode.Impulse);
