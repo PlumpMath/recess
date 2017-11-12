f991119ee57d77bb19b017f71d28cd3d340b9fa0
Matthew Chase <antishow@gmail.com>
2017-11-12 18:23:10 -0500

    Starting on replacing the dev network ui

---
+++ Assets/Scripts/NetworkManagerSingleton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManagerSingleton : NetworkBehaviour {
+	void Awake(){
+		if(NetworkManager.singleton){
+			Destroy(gameObject);
+		}
+	}
+
+	public override void OnStartLocalPlayer(){
+		Debug.Log("START LOCAL PLAYER!");
+	}
+}
--- Assets/Scripts/TitleScreenController.cs
+++ Assets/Scripts/TitleScreenController.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 public class TitleScreenController : MonoBehaviour {
+	public NetworkManager networkManager;
 
-	public void OnClickNewGame(){
-		Debug.Log("New Game");
+	public void OnClickHostGame(){
+		Debug.Log("Host a Game!");
+		networkManager.StartHost();
 		SceneManager.LoadScene("Main");
 	}
 
+	public void OnClickJoinGame(){
+		Debug.Log("Join a Game!");
+	}
+
+	public void OnClickEditCharacter(){
+		Debug.Log("Edit Character!");
+	}
+
 	public void OnClickQuit(){
-		Debug.Log("Quit");
+		Debug.Log("Quit!");
 		Application.Quit();
 	}
 }
