113751e33fa5a95f0358904699dadc98aa46812f
Matthew Chase <antishow@gmail.com>
2017-11-13 20:42:45 -0500

    Simplify Title Screen

--- Assets/Scripts/TitleScreenController.cs
+++ Assets/Scripts/TitleScreenController.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Networking;
 
 public class TitleScreenController : MonoBehaviour {
-	public NetworkManager networkManager;
 
 	public void OnClickStartGame(){
 		SceneManager.LoadScene("Game Lobby");
