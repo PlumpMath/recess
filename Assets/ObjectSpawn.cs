30eb4374284f6188ef550878a911b8582e0ae4d3
Matthew Chase <antishow@gmail.com>
2017-11-20 13:29:52 -0500

    spawn a ball instead of having it in the scene

---
+++ Assets/ObjectSpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ObjectSpawn : MonoBehaviour {
+	public GameObject prefab;
+
+	void Start(){
+		GameObject o = Instantiate(prefab, transform.position, transform.rotation);
+		NetworkServer.Spawn(o);
+
+		Destroy(gameObject);
+	}
+}
