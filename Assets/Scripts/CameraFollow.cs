8ea9e8cfca4584297da15413cc00dcabf22ed5a1
Matthew Chase <antishow@gmail.com>
2017-11-22 15:22:40 -0500

    Camera will now follow the player immediate if it drops below "sea level" instead of waiting for it to touch the ground

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,6 +10,7 @@ public class CameraFollow : NetworkBehaviour {
     public float cameraMinY = -0.5f;
     public float cameraMaxY = 6.0f;
     private Camera mainCamera;
+    private GameObject[] ObscuringPlayer;
 
     private float _groundLevel  = 1.0f;
     public float GroundLevel{
@@ -22,19 +23,21 @@ public class CameraFollow : NetworkBehaviour {
     public float CameraAngle = 0.0f;
     public float CameraSpeed = 18.0f;
 
+    Vector3 TargetPosition(){
+        return new Vector3(
+            transform.position.x,
+            Mathf.Min(transform.position.y, GroundLevel),
+            transform.position.z
+        );
+    }
+
     Vector3 CameraPosition()
     {
         float t = Mathf.Repeat(CameraAngle + Mathf.PI, Mathf.PI * 2.0f);
         float discY = Mathf.Lerp(cameraMinY, cameraMaxY, CameraDistance);
         float discR = Mathf.Lerp(cameraMinX, cameraMaxX, CameraDistance);
 
-        Vector3 me = new Vector3(
-            transform.position.x,
-            GroundLevel,
-            transform.position.z
-        );
-
-        return me + new Vector3(
+        return TargetPosition() + new Vector3(
             Mathf.Sin(t) * discR,
             discY,
             Mathf.Cos(t) * discR
@@ -76,6 +79,8 @@ public class CameraFollow : NetworkBehaviour {
         CameraDistance = Mathf.Clamp01(CameraDistance + cameraInput.y);
 
         mainCamera.transform.position = CameraPosition();
-        mainCamera.transform.LookAt(new Vector3(transform.position.x, GroundLevel, transform.position.z));
+        mainCamera.transform.LookAt(TargetPosition());
+
+
 	}
 }
