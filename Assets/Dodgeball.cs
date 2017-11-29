c8092076e32e03016a58f5887baff4efa6ebc4ef
Matthew Chase <antishow@gmail.com>
2017-11-29 13:06:01 -0500

    Make the dodgeball play the bounce sound when it touches something

---
+++ Assets/Dodgeball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dodgeball : MonoBehaviour {
+	private AudioSource bounce;
+
+	void Awake(){
+		bounce = GetComponent<AudioSource>();
+	}
+
+	void OnCollisionEnter(Collision other){
+		bounce.Play();
+	}
+}
