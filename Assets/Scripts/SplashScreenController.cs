6e8bdc64c03d90fdfc64c0bbe40870b5e95ee35f
Matthew Chase <antishow@gmail.com>
2017-12-01 10:32:54 -0500

    Add Intro Screen

---
+++ Assets/Scripts/SplashScreenController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashScreenController : MonoBehaviour {
+	public void Complete(){
+		SceneManager.LoadScene("Title Screen");
+	}
+
+	public void SayVital(){
+		GetComponent<AudioSource>().Play();
+	}
+}
